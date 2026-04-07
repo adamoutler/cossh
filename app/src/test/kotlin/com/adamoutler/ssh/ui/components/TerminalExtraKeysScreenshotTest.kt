7132825cf545b23e5b518b412ef98e23a5ab1876
Adam Outler <adamoutler@gmail.com>
2026-04-07 19:06:51 -0400

    test: Add Paparazzi visual tests for TerminalExtraKeys panel

---
+++ app/src/test/kotlin/com/adamoutler/ssh/ui/components/TerminalExtraKeysScreenshotTest.kt
@@ -0,0 +1,47 @@
+package com.adamoutler.ssh.ui.components
+
+import app.cash.paparazzi.DeviceConfig.Companion.PIXEL_5
+import app.cash.paparazzi.Paparazzi
+import org.junit.Rule
+import org.junit.Test
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+
+class TerminalExtraKeysScreenshotTest {
+
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = PIXEL_5,
+        theme = "android:Theme.Material.Light.NoActionBar"
+    )
+
+    @Test
+    fun page1_noModifiers() {
+        paparazzi.snapshot {
+            TerminalExtraKeys(
+                ctrlActive = false,
+                altActive = false,
+                superActive = false,
+                menuActive = false,
+                onKeyToggle = {},
+                onKeyPress = {},
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+
+    @Test
+    fun page1_withModifiers() {
+        paparazzi.snapshot {
+            TerminalExtraKeys(
+                ctrlActive = true,
+                altActive = false,
+                superActive = true,
+                menuActive = false,
+                onKeyToggle = {},
+                onKeyPress = {},
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
