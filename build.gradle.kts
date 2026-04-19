6086f1a71f124ee25f33324c6088e60f05c1b9ca
Adam Outler <adamoutler@gmail.com>
2026-04-18 20:08:15 -0400

    Also bump BouncyCastle pkix and util artifacts to 1.84 to fix ClassNotFoundException

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ buildscript {
         classpath("io.netty:netty-codec-http:4.1.108.Final")
         classpath("org.bitbucket.b_c:jose4j:0.9.4")
         classpath("org.bouncycastle:bcprov-jdk18on:1.84")
+        classpath("org.bouncycastle:bcpkix-jdk18on:1.84")
+        classpath("org.bouncycastle:bcutil-jdk18on:1.84")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
