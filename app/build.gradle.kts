57abca2a484a4773f978c3c47421bfa5169c52a9
Adam Outler <adamoutler@gmail.com>
2026-04-19 08:08:14 -0400

    build: exclude io.netty.versions.properties from packaging to fix assembleDebug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ android {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
             excludes += "META-INF/DEPENDENCIES"
+            excludes += "META-INF/INDEX.LIST"
+            excludes += "META-INF/io.netty.versions.properties"
             excludes += "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
         }
     }
