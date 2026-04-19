ae24160fe81e73adfde420666fdef1e58fcc4b81
Adam Outler <adamoutler@gmail.com>
2026-04-18 23:45:36 -0400

    build: force resolution of vulnerable dependabot dependencies in all buildscripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,32 @@ buildscript {
         classpath("commons-io:commons-io:2.14.0")
         classpath("com.google.protobuf:protobuf-java:3.25.5")
     }
+    configurations.classpath {
+        resolutionStrategy {
+            force("io.netty:netty-codec-http:4.1.132.Final")
+            force("io.netty:netty-codec-http2:4.1.132.Final")
+            force("io.netty:netty-codec:4.1.132.Final")
+            force("io.netty:netty-common:4.1.132.Final")
+            force("io.netty:netty-handler:4.1.132.Final")
+            force("io.netty:netty-transport:4.1.132.Final")
+            force("io.netty:netty-buffer:4.1.132.Final")
+            force("io.netty:netty-resolver:4.1.132.Final")
+            force("io.netty:netty-transport-native-unix-common:4.1.132.Final")
+            force("org.bitbucket.b_c:jose4j:0.9.6")
+            force("org.bouncycastle:bcprov-jdk18on:1.84")
+            force("org.bouncycastle:bcpkix-jdk18on:1.84")
+            force("org.bouncycastle:bcutil-jdk18on:1.84")
+            force("org.jdom:jdom2:2.0.6.1")
+            force("commons-io:commons-io:2.14.0")
+            force("com.google.protobuf:protobuf-java:3.25.5")
+            
+            dependencySubstitution {
+                substitute(module("org.bouncycastle:bcprov-jdk15on")).using(module("org.bouncycastle:bcprov-jdk18on:1.84"))
+                substitute(module("org.bouncycastle:bcpkix-jdk15on")).using(module("org.bouncycastle:bcpkix-jdk18on:1.84"))
+                substitute(module("org.bouncycastle:bcutil-jdk15on")).using(module("org.bouncycastle:bcutil-jdk18on:1.84"))
+            }
+        }
+    }
 }
 
 allprojects {
