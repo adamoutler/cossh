56db5d0a66f170fcba40222d2c46244a6a0eff6d
Adam Outler <adamoutler@gmail.com>
2026-04-22 07:54:04 -0400

    Fix UserJourneyIntegrationTest by removing manifest=Config.NONE

--- app/src/test/kotlin/com/adamoutler/ssh/ui/UserJourneyIntegrationTest.kt
+++ app/src/test/kotlin/com/adamoutler/ssh/ui/UserJourneyIntegrationTest.kt
@@ -11,7 +11,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [34], manifest = Config.NONE)
+@Config(sdk = [34])
 class UserJourneyIntegrationTest {
 
     @get:Rule
