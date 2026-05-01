7a64a772dc649723ba0fa071a0883fd04c462485
Adam Outler <adamoutler@gmail.com>
2026-04-30 20:21:11 -0400

    Test: Add integration test for SectionHeaderWithInfo intent (SSH-130)

---
+++ app/src/test/kotlin/com/adamoutler/ssh/ui/screens/SectionHeaderWithInfoTest.kt
@@ -0,0 +1,41 @@
+package com.adamoutler.ssh.ui.screens
+
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.platform.UriHandler
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
+import androidx.compose.runtime.CompositionLocalProvider
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.junit.Assert.assertEquals
+
+@RunWith(RobolectricTestRunner::class)
+class SectionHeaderWithInfoTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testInfoIconOpensWiki() {
+        var openedUri: String? = null
+        val testUriHandler = object : UriHandler {
+            override fun openUri(uri: String) {
+                openedUri = uri
+            }
+        }
+
+        composeTestRule.setContent {
+            CompositionLocalProvider(LocalUriHandler provides testUriHandler) {
+                SectionHeaderWithInfo(title = "Test", topic = "TestTopic")
+            }
+        }
+
+        composeTestRule.onNodeWithContentDescription("Learn more about TestTopic. Opens external browser.")
+            .performClick()
+
+        assertEquals("https://github.com/adamoutler/ssh/wiki", openedUri)
+    }
+}
