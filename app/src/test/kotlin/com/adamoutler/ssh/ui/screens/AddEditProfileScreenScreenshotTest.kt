b13f223a7caf6610a7e1ecda64207ee35d6bb347
Adam Outler <adamoutler@gmail.com>
2026-05-14 16:38:36 -0400

    test: Add Dynamic Port Forwarding screenshot test and QA docs (SSH-175)

--- app/src/test/kotlin/com/adamoutler/ssh/ui/screens/AddEditProfileScreenScreenshotTest.kt
+++ app/src/test/kotlin/com/adamoutler/ssh/ui/screens/AddEditProfileScreenScreenshotTest.kt
@@ -405,4 +405,62 @@ class AddEditProfileScreenScreenshotTest {
             }
         }
     }
+
+    @Test
+    fun portForwardDynamicDialogScreen() {
+        paparazzi.snapshot {
+            CoSSHTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    AddEditProfileScreenContent(
+                        profileId = null,
+                        nickname = "Test Server",
+                        onNicknameChange = {},
+                        host = "test.example.com",
+                        onHostChange = {},
+                        port = "22",
+                        onPortChange = {},
+                        protocol = com.adamoutler.ssh.data.Protocol.SSH,
+                        onProtocolChange = {},
+                        username = "ubuntu",
+                        onUsernameChange = {},
+                        password = "secure_password",
+                        onPasswordChange = {},
+                        isPasswordLocked = false,
+                        onPasswordLockedChange = {},
+                        authType = AuthType.PASSWORD,
+                        onAuthTypeChange = {},
+                        availableKeys = emptyList(),
+                        keyReference = "",
+                        onKeyReferenceChange = {},
+                        identities = emptyList(),
+                        identityId = null,
+                        onIdentityChange = {},
+                        onManageIdentities = {},
+                        envVarsText = "",
+                        onEnvVarsTextChange = {},
+                        portForwards = emptyList(),
+                        onPortForwardsChange = {},
+                        initialDirectory = "",
+                        onInitialDirectoryChange = {},
+                        terminalInputState = 0,
+                        onTerminalInputStateChange = {},
+                        keepScreenOnMode = com.adamoutler.ssh.data.KeepScreenOnMode.SYSTEM_DEFAULT,
+                        onKeepScreenOnModeChange = {},
+                        useLocalDns = false,
+                        onUseLocalDnsChange = {},
+                        commandSnippets = emptyList(),
+                        onCommandSnippetsChange = {},
+                        onSave = {},
+                        onNavigateBack = {},
+                        defaultPasswordVisible = true,
+                        defaultShowAddDialog = true,
+                        defaultPortForwardType = com.adamoutler.ssh.data.PortForwardType.DYNAMIC,
+                    )
+                }
+            }
+        }
+    }
 }
